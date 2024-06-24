c342e2fd07f6a3bcc5a80ed3109cf5c3f7adb863
lorloop <gallet.julien@orange.fr>
2024-06-24 23:10:26 +0200

    error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,3 +1,12 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
 export default function Error() {
-    return <p>Error works !</p>
+  return (
+    <div className="error">
+      <p className="error__code">404</p>
+      <p className="error__description">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </div>
+  )
 }
